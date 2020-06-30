2629ea7e54700a9649a69a6f2ca66b0310708a9c
IlluSion <georgescu.calin.stefan@gmail.com>
2020-06-30 17:03:47 +0300

    Added fuctions for reading/writing

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+FILE *fin, *fout;
+
+int width;
+int height;
+size_t bytes;
+char format;
+
+void open_files () {
+    fin = fopen ( "video_in.bw", "rb" );
+    fout = fopen ( "video_out.bw", "wb" );
+}
+
+int getFrame ( char **frame ) { 
+    return fread ( &frame, bytes, width*height, fin );
+}
+
+void convertFrame ( char **frame ) {
+
+}
+
+void printFrame ( char **frame ) {
+    fwrite ( frame, bytes, width*height, fout );
+}
+
 int main ( int argc, char* argv[] ) {
+
+    open_files();
+
+    char **frame;
+
+    while ( getFrame ( frame ) == width*height ) {
+        convertFrame ( frame );
+        printFrame ( frame );
+    }
 
     return 0;
 }
