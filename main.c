7c8f0f3b21e902d290addf239df08c19b3417868
iulian.palade@xperi.com <Iulian.Palade@xperi.com>
2020-07-01 09:39:13 +0300

    Example of improvment.

--- main.c
+++ main.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
 
+#define IN
+#define OUT
+
+typedef signed char Int8;
+typedef signed short Int16;
+typedef signed int Int32;
+
+
+typedef unsigned char UInt8;
+typedef unsigned short UInt16;
+typedef unsigned int UInt32;
+
+#define IMG_WIDTH 100
+#define IMG_HEIGHT 100
+
+
+
+// Image.h
+
+typedef struct Image
+{
+    UInt32 width;
+    UInt32 height;
+    UInt8* data;
+    UInt8 bpp;
+    UInt8 format;
+    
+} Image;
+
+int CreateImage(
+    IN UInt8 format,
+    IN UInt8 bpp,
+    IN UInt32 width,
+    IN UInt32 height,
+    OUT Image** img);
+    
+void DestroyImage(
+    IN OUT Image** img);
+    
+// Image.c
+int CreateImage(
+    IN UInt8 format,
+    IN UInt8 bpp,
+    IN UInt32 width,
+    IN UInt32 height,
+    OUT Image* img)
+{
+}
+
+
 FILE *fin, *fout;
 
 int width;
@@ -24,11 +74,32 @@ void printFrame ( char **frame ) {
     fwrite ( frame, bytes, width*height, fout );
 }
 
+gcc main.c image.c -o exec
+
 int main ( int argc, char* argv[] ) {
+
+    Image* img = NULL;
+    CreateImage(&img);
+
+    
+    UInt8* img;
+    img = malloc(IMG_WIDTH * IMG_HEIGHT * sizeof(UInt8));
+    
+    for(int y = 0; y < IMG_HEIGHT; y++)
+    {
+        for(int x = 0; x < IMG_WIDTH; x++)
+        {
+            UInt8 val = img[y * IMG_WIDTH + x];
+        }
+    }
+    
+
+    
+    
 
     open_files();
 
-    char **frame;
+    char** frame;
 
     while ( getFrame ( frame ) == width*height ) {
         convertFrame ( frame );
